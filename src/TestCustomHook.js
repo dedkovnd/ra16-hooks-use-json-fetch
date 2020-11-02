189c644ddb6ed0c9b8d467b3b4116906d4519e18
dedkovnd <dedkovnd@gmail.com>
2020-11-02 18:03:40 +0300

    add isLoading

--- src/TestCustomHook.js
+++ src/TestCustomHook.js
@@ -1,23 +1,41 @@
 import React,{useState,useEffect} from "react";
 
-function useJsonFetch(url) {
-    const [data, setData] = useState()
-    const [error, setError] = useState()
+// const url = 'http://localhost:7070/data'
+// const err = 'http://localhost:7070/error'
+// const loading = 'http://localhost:7070/loading' - тестовые ссылки
 
-    fetch(url)
-        .then(response=> response.json())
-        .then(json=>setData(json))
-        .catch((e)=>setError(e.message))
+function useJsonFetch(url,opt) {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(()=> {
+       const fetchData = async ()=> {
+           setLoading(true)
+           try {
+               const response = await fetch(url)
+               const data = await response.json()
+               if (response.ok) {
+                   setData(data)
+               }
+           } catch(e) {
+               setError(e)
+           } finally {
+               setLoading(false)
+           }
+       };
+       fetchData()
+    },[url])
+
+    return[{data,isLoading,error}]
 }
 
-export function TestCustomHook(){
-    const url = 'http://localhost:7070/data'
-    const err = 'http://localhost:7070/error'
-    const loading = 'http://localhost:7070/loading'
-
-    useJsonFetch(url)
+export function TestCustomHook(props){
+    const[{data,isLoading,error}] = useJsonFetch(props,{})
 
     return (
-        <>Test</>
+        <div>Test
+            {isLoading ? <span>Loading...</span> : <span>{data}</span> || <span>{error}</span>}
+        </div>
     )
 }
